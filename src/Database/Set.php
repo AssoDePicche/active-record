b58a769ff56513727cc0585b9e980b9acf48e11b
AssoDePicche <samuelprado730@gmail.com>
2023-05-24 00:33:05 -0300

    feat: add set class

---
+++ src/Database/Set.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database;
+
+use ArrayIterator;
+use Traversable;
+
+final class Set implements Collection
+{
+    private array $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function add(mixed ...$item): void
+    {
+        array_push($this->data, ...$item);
+    }
+
+    public function clear(): void
+    {
+        $this->data = [];
+    }
+
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    public function isEmpty(): bool
+    {
+        return ($this->count() === 0);
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->data);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->data;
+    }
+}
