55d8c6547007acc8d6c3d402014a443ea9af204d
AssoDePicche <samuelprado730@gmail.com>
2023-05-25 00:01:23 -0300

    feat: add sort method

--- src/Database/Collection.php
+++ src/Database/Collection.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Database;
 
-use IteratorAggregate;
-use JsonSerializable;
-
-interface Collection extends \Countable, IteratorAggregate, JsonSerializable
+interface Collection extends \Countable, \IteratorAggregate, \JsonSerializable
 {
     public function add(mixed ...$item): void;
 
     public function clear(): void;
 
     public function isEmpty(): bool;
+
+    public function sort(): void;
 }
