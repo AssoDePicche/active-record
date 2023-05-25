e5aec256550981109947ddeb8b66df1cfc366bcd
AssoDePicche <samuelprado730@gmail.com>
2023-05-25 00:02:54 -0300

    feat: add database interface

---
+++ src/Database/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database;
+
+interface Database
+{
+    public function __construct(string $dsn, ?string $username = null, ?string $password = null);
+    public function prepare(string $query): self;
+
+    public function execute(?array $params = null): bool;
+
+    public function fetch(?string $class = null): object|false;
+
+    public function fetchAll(?string $class = null): Collection;
+
+    public function rowCount(): int;
+
+    public function inTransaction(): bool;
+
+    public function beginTransaction(): bool;
+
+    public function commit(): bool;
+
+    public function rollBack(): bool;
+
+    public static function createFromEnv(): self;
+
+    public static function getInstance(): self;
+}
