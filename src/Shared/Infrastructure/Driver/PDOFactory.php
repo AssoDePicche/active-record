1c98bd14a7cf25d3420b26167e10366bdbe16ed5
AssoDePicche <samuelprado730@gmail.com>
2023-06-01 23:27:51 -0300

    feat: add pdo factory

---
+++ src/Shared/Infrastructure/Driver/PDOFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\Adapter\Infrastructure\Driver;
+
+use PDO;
+
+final class PDOFactory
+{
+    public static function createFromEnv(): ?PDO
+    {
+        $dsn = sprintf(
+            '%s:host=%s:%d;dbname=%s',
+            $_ENV['DB_CONNECTION'],
+            $_ENV['DB_HOST'],
+            $_ENV['DB_PORT'],
+            $_ENV['DB_DATABASE']
+        );
+
+        return new PDO(
+            $dsn,
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD']
+        );
+    }
+}
