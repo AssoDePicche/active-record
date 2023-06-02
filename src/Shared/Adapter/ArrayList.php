5d5b2c20ab41ff693b7926f5d0f52aa787f625a0
AssoDePicche <samuelprado730@gmail.com>
2023-06-01 23:26:21 -0300

    refactor: update set to an array list

---
+++ src/Shared/Adapter/ArrayList.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\Adapter;
+
+use Shared\Adapter\Contract\Collection;
+
+final class ArrayList implements Collection
+{
+    private array $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function add(mixed ...$item): void
+    {
+        array_push($this->data, ...$item);
+    }
+
+    public function clear(): void
+    {
+        $this->data = [];
+    }
+
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    public function isEmpty(): bool
+    {
+        return ($this->count() === 0);
+    }
+
+    public function sort(): void
+    {
+        sort($this->data);
+    }
+
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->data);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->data;
+    }
+}
